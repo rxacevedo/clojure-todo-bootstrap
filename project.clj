8fd14858b38b0e704680ab2ee3c4432c724cdb5b
Roberto Acevedo <rxacevedo@gmail.com>
2014-05-08 06:58:54 -0400

    Heroku things

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.2"]
                  [compojure "1.1.7"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "webdev.jar"
   :main webdev.core
   :profiles {:dev {:main webdev.core/-dev-main}})
