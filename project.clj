4f65103a41e0651b9ec07b4ce885e0746c96e107
Roberto Acevedo <rxacevedo@gmail.com>
2014-05-21 07:26:18 -0400

    Bring in hiccup, not sure how it compiled without it (does compojure include it?), aot all the things, use jquery 1.110 to get rid of event.returnValue deprecation warning.

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject webdev "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure webdev tutorial project"
+  :url "http://cljthing.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.2"]
                  [compojure "1.1.7"]
+                 [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
   :min-lein-version "2.0.0"
   :uberjar-name "webdev.jar"
   :repl-options {:init-ns user}
-  :main webdev.core/main
-  :profiles {:dev {:main webdev.core/-dev-main}})
+  :main webdev.core
+  :profiles {:uberjar {:aot :all}})
--- src/webdev/view.clj
+++ src/webdev/view.clj
@@ -46,5 +46,5 @@
            [:header
             [:h1 "Hi there, sailor!"]]
            [:footer
-            [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"}]
+            [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"}]
             [:script {:src "/bootstrap/js/bootstrap.min.js"}]]]]))
